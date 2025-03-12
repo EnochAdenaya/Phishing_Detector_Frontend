23e419e011fa7216157533e333015f62ba6952e3
EnochAdenaya <enochadenaya16@gmail.com>
2025-03-13 00:20:24 +0100

    Updated the endpoint

--- react-app/src/phishingDetector.jsx
+++ react-app/src/phishingDetector.jsx
@@ -10,7 +10,7 @@ const PhishingDetector = () => {
     const checkEmail = async () => {
         setError(null); // Reset error before making a request
         try {
-            const response = await axios.post("http://127.0.0.1:5000/predict", {
+            const response = await axios.post("https://phishing-detector-backend-cjxp.onrender.com/predict", {
                 email: email
             });
             setResult(response.data);
