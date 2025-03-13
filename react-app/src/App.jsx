c19ce7e0948fbce093362563d87834e019288314
EnochAdenaya <enochadenaya16@gmail.com>
2025-03-13 10:11:32 +0100

    Testin the pinging of my backend

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,24 +2,24 @@ import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./appRouter";
 
-const BACKEND_URL = "https://phishing-detector-backend-cjxp.onrender.com/";
+// const BACKEND_URL = "https://phishing-detector-backend-cjxp.onrender.com/";
 
 function App() {
-    useEffect(() => {
-        const pingBackend = () => {
-            fetch(BACKEND_URL)
-                .then((res) => console.log("Backend Pinged:", res.status))
-                .catch((err) => console.error("Ping failed:", err));
-        };
+//     useEffect(() => {
+//         const pingBackend = () => {
+//             fetch(BACKEND_URL)
+//                 .then((res) => console.log("Backend Pinged:", res.status))
+//                 .catch((err) => console.error("Ping failed:", err));
+//         };
 
-        // Ping every 5 minutes (300,000ms)
-        const interval = setInterval(pingBackend, 5 * 60 * 1000);
+//         // Ping every 5 minutes (300,000ms)
+//         const interval = setInterval(pingBackend, 5 * 60 * 1000);
 
-        // Initial ping on component mount
-        pingBackend();
+//         // Initial ping on component mount
+//         pingBackend();
 
-        return () => clearInterval(interval); // Cleanup on unmount
-    }, []);
+//         return () => clearInterval(interval); // Cleanup on unmount
+//     }, []);
 
     return (
         <BrowserRouter>
