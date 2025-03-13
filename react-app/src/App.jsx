2dbecfbd0f4972bcc99515a8fce19251ca78c431
EnochAdenaya <enochadenaya16@gmail.com>
2025-03-13 09:54:15 +0100

    Updated App.jsx to continously ping the backend

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./appRouter";
 
+const BACKEND_URL = "https://phishing-detector-backend-cjxp.onrender.com/";
+
 function App() {
+    useEffect(() => {
+        const pingBackend = () => {
+            fetch(BACKEND_URL)
+                .then((res) => console.log("Backend Pinged:", res.status))
+                .catch((err) => console.error("Ping failed:", err));
+        };
+
+        // Ping every 5 minutes (300,000ms)
+        const interval = setInterval(pingBackend, 5 * 60 * 1000);
+
+        // Initial ping on component mount
+        pingBackend();
+
+        return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
     return (
         <BrowserRouter>
             <AppRouter />
