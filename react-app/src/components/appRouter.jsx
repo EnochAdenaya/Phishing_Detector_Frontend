4776a75845c18efbd63e2a840421d2555200ed9a
EnochAdenaya <enochadenaya16@gmail.com>
2025-03-16 23:40:29 +0100

    Added Notification Page

--- react-app/src/components/appRouter.jsx
+++ react-app/src/components/appRouter.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./dashboard";  // Your Home Page
-import PhishingDetector from "./phishingDetector"; 
-import SignIn from "./signIn"; 
+import Dashboard from "./dashboard/dashboard";  // Your Home Page
+import PhishingDetector from "./phishingDetector/phishingDetector"; 
+import Authentication from "./authentication"; 
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/" element={<Dashboard />} />  
             <Route path="/phishing" element={<PhishingDetector />} />
-            <Route path="/auth" element={<SignIn />} />
+            <Route path="/auth" element={<Authentication />} />
+            {/* <Route path="/notification" element={<Notification />}/> */}
         </Routes>
     );
 };
--- react-app/src/components/dashboard/dashboard.jsx
+++ react-app/src/components/dashboard/dashboard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { auth } from "./firebaseConfig.jsx";
+import { auth } from "../firebaseConfig.jsx";
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import "./dashboard.css"
 
